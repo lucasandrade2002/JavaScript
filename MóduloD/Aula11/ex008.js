ed7c744c4883c6503c6f4d0925aa4172ca30b7e6
Lucas <lucas.andradec2002@gmail.com>
2022-05-19 13:47:12 -0300

    Exercícios aula 11

---
+++ MóduloD/Aula11/ex008.js
@@ -0,0 +1,10 @@
+
+let velocidade = 70
+
+console.log(`A velocidade detectada pelo radar foi de ${velocidade}km/h.`)
+
+if (velocidade > 60) {
+    console.log("Você foi multado por excesso de velocidade.")
+}
+
+console.log("Dirija sempre com cinto de segurança :)")
